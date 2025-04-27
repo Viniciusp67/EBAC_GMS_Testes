daead5be2748f3e8d3301a9db5ccdbf75c458ef1
paulo <vinicius.paulo348@gmail.com>
2025-04-27 17:12:51 -0300

    refatoração e teste de busca

---
+++ cypress/e2e/busca.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/>
+
+describe('US-001 : Funcionalidade : Busca de filmes', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    });
+
+    it('Deve buscar filmes com sucesso', () => {
+        cy.get('#search-input').type('Matrix')
+        cy.get('#search-button').click()
+        cy.get('#results-section').should('contain', 'Matrix')
+    });
+
+    it('Deve buscar filmes com sucesso de uma lista', () => {
+        cy.fixture("filmes").then((filmes) =>{
+            cy.get('#search-input').type(filmes[5].titulo)
+            cy.get('#search-button').click()
+            cy.get('#results-section').should('contain', filmes[5].titulo)
+    })
+    });
+
+    it.only('Deve buscar filmes com sucesso da  lista iinteira', () => {
+        cy.fixture("filmes").each((filmes) =>{
+            cy.get('#search-input').clear().type(filmes.titulo)
+            cy.get('#search-button').click()
+            cy.get('#results-section').should('contain', filmes.titulo)
+    })
+    });
+
+});
---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress"/>
+
+describe('US-012-Funcionalidade: Cadastro de membros', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  }
+
+);
+
+  it('Cadastro de campos obrigatórios', () => {
+    var email = `paulo${Date.now()}@teste.com`
+    cy.preencherCadastro('Paulo', 'Melo', email,'12345678998', 'Teste@852')
+    cy.get('#signup-response').should("contain", "Cadastro realizado com sucesso!")
+  })
+
+  it('Valdiar mensagem de erro com o campo nome inválido', () => {
+    cy.preencherCadastro('Paulo20', 'Melo', "paulo@teste.com", '11985642378','Teste@852')
+    cy.get('#signup-response').should('contain', "Nome deve conter apenas caracteres alfabéticos, acentuados e espaços")
+});
+
+  it('Validar mensagem de erro com o campo sobrenome inválido', () => {
+    cy.preencherCadastro('Paulo', 'Melo123', "paulo@teste.com", '11985642378', 'Teste@852')
+    cy.get('#signup-response').should('contain', "Sobrenome deve conter apenas caracteres alfabéticos, acentuados e espaços")
+  })
+
+  it('Validar mensagem de erro com o campo email inválido', () => {
+    cy.preencherCadastro('Paulo', 'Melo', "pauloemail.com", '11985642378', 'Teste@852')
+    cy.get('#signup-response').should('contain', "Informe um e-mail válido")
+  })
+
+  it('Validar mensagem de erro com o campo telefone inválido', () => {
+    cy.preencherCadastro('Paulo', 'Melo', "paulo@teste.com", 'telefone', 'Teste@852')
+    cy.get('#signup-response').should('contain', "Telefone deve conter apenas números")
+  })
+
+  it('Validar mensagem de erro com o campo senha inválido', () => {
+    cy.preencherCadastro('Paulo', 'Melo', "paulo@teste.com", '11985642378', '123')
+    cy.get('#signup-response').should('contain', "A senha deve conter no mínimo 6 caracteres, letras e números")
+  })
+
+
+
+})
